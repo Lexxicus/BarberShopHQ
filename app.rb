cc43c94791e97c9a135d5a679214b59698c64840
Lexxicus <alexxxicus@gmail.com>
2019-07-19 12:09:26 +0500

    add Client entity

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'rubygems'
 require 'sinatra'
 require "bundler/setup"
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+
+end
 
 
 get '/' do
